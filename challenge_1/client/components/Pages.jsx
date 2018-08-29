2d0178307bd69c2543078ada7c10abe094f84819
Ben Cronin <benc@Bens-MacBook-Pro.local>
2018-08-29 16:02:27 -0700

    Implement dynamic display of available page numbers for navigation

--- challenge_1/client/components/Pages.jsx
+++ challenge_1/client/components/Pages.jsx
@@ -1,12 +1,21 @@
 import React from "react";
 
+const showPage = (val, currentPage, lastPage) =>
+  val <= 2 || Math.abs(val - currentPage) <= 2 || val >= lastPage - 2;
+
 export default ({ currentPage, lastPage, changePage }) => {
-  const pages = [...Array(lastPage).keys()];
+  const pages = [...Array(lastPage).keys()].map(val => val + 1);
   return (
     <div>
-      {pages.map((val, idx) => (
-        <div onClick={() => changePage(val)}>{val}</div>
-      ))}
+      {pages.map((val, idx) => {
+        if (showPage(val, currentPage, lastPage)) {
+          return (
+            <div onClick={() => changePage(val)}>
+              {val} {!showPage(val + 1, currentPage, lastPage) ? "..." : ""}
+            </div>
+          );
+        }
+      })}
     </div>
   );
 };
