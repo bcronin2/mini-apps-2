6149e88dd1c0ab1af5a31e72e5f449140f129be7
Ben Cronin <benc@Bens-MacBook-Pro.local>
2018-08-29 12:10:07 -0700

    Create basic SearchHistory component and connect to json-server

--- challenge_1/client/app.jsx
+++ challenge_1/client/app.jsx
@@ -1,6 +1,36 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import axios from "axios";
 
-const HelloWorld = () => <div>Hello, world!</div>;
+import ENDPOINTS from "./endpoints";
 
-ReactDOM.render(<HelloWorld />, document.getElementById("container"));
+class SearchHistory extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchResults: []
+    };
+    this.handleSearch = this.handleSearch.bind(this);
+  }
+
+  handleSearch(e) {
+    const { value } = e.target;
+    axios.get(ENDPOINTS.searchApi).then(results => {
+      this.setState({ searchResults: results.data });
+    });
+  }
+
+  render() {
+    const { searchResults } = this.state;
+    return (
+      <div>
+        <input onChange={this.handleSearch} />
+        {searchResults.map(searchResult => (
+          <div>{JSON.stringify(searchResult)}</div>
+        ))}
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<SearchHistory />, document.getElementById("container"));
