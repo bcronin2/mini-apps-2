4ed2361b010b2955260e99d7188ca6368b0bb3b1
Ben Cronin <benc@Bens-MacBook-Pro.local>
2018-08-30 15:48:59 -0700

    Add basic styling to user input component

--- challenge_2/client/components/DateSelection.jsx
+++ challenge_2/client/components/DateSelection.jsx
@@ -6,32 +6,63 @@ export default class DateSelection extends React.Component {
     const { startDate, endDate } = props;
     this.state = {
       startDate,
-      endDate
+      endDate,
+      isValid: true
     };
+    this.updateStartDate = this.updateStartDate.bind(this);
+    this.updateEndDate = this.updateEndDate.bind(this);
+  }
+
+  validateDates(startDate, endDate, callback) {
+    const startTime = new Date(startDate).getTime();
+    const endTime = new Date(endDate).getTime();
+    const now = new Date().getTime();
+    const isValid = startTime < endTime && endTime <= now;
+    this.setState({ isValid }, callback);
+  }
+
+  updateStartDate(e) {
+    const { value } = e.target;
+    const { endDate } = this.state;
+    this.validateDates(value, endDate, () => {
+      this.setState({ startDate: value });
+    });
+  }
+
+  updateEndDate(e) {
+    const { value } = e.target;
+    const { startDate } = this.state;
+    this.validateDates(startDate, value, () => {
+      this.setState({ endDate: value });
+    });
   }
 
   render() {
     const { handleUpdate } = this.props;
-    const { startDate, endDate } = this.state;
+    const { startDate, endDate, isValid } = this.state;
     return (
-      <div>
-        Start date:{' '}
-        <input
-          type="date"
-          value={startDate}
-          onChange={e => this.setState({ startDate: e.target.value })}
-        />
-        End date:{' '}
-        <input
-          type="date"
-          value={endDate}
-          onChange={e => this.setState({ endDate: e.target.value })}
-        />
-        <input
-          type="button"
-          label="Go!"
-          onClick={() => handleUpdate(startDate, endDate)}
-        />
+      <div className="user-input">
+        <span>
+          Start date:{' '}
+          <input
+            type="date"
+            value={startDate}
+            onChange={this.updateStartDate}
+          />
+        </span>
+        <span>
+          End date:{' '}
+          <input type="date" value={endDate} onChange={this.updateEndDate} />
+        </span>
+        {isValid ? (
+          <input
+            type="button"
+            value="Go!"
+            onClick={() => handleUpdate(startDate, endDate)}
+          />
+        ) : (
+          <span>Oops! Those dates aren't valid</span>
+        )}
       </div>
     );
   }
--- challenge_2/client/components/PriceChart.jsx
+++ challenge_2/client/components/PriceChart.jsx
@@ -4,6 +4,6 @@ import { Line } from 'react-chartjs-2';
 export default ({ prices }) => {
   const labels = Object.keys(prices);
   const data = Object.values(prices);
-  const chartData = { labels, datasets: [{ data, label: 'Bitcoin Prices' }] };
+  const chartData = { labels, datasets: [{ data, label: 'Bitcoin Price' }] };
   return <Line data={chartData} />;
 };
