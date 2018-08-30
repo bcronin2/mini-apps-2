6a374fd4aefd91968ddeb96895c71f4242557967
Ben Cronin <benc@Bens-MacBook-Pro.local>
2018-08-30 12:41:31 -0700

    Use datejs to format dates for API  calls

--- challenge_2/client/components/App.jsx
+++ challenge_2/client/components/App.jsx
@@ -1,20 +1,36 @@
 import React from 'react';
 import PriceChart from './PriceChart.jsx';
+import 'datejs';
 
 import coinDesk from '../API/coinDeskAPI.js';
 
-const startDate = '2018-04-22';
-const endDate = '2018-08-22';
+const TODAY = Date.today().toString('yyyy-MM-dd');
+const WEEK_AGO = Date.today()
+  .addWeeks(-1)
+  .toString('yyyy-MM-dd');
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      startDate: WEEK_AGO,
+      endDate: TODAY,
       prices: {}
     };
+    this.updateDates = this.updateDates.bind(this);
   }
 
   componentDidMount() {
+    this.getPrices();
+  }
+
+  updateDates(startDate, endDate) {
+    this.setState({ startDate, endDate });
+  }
+
+  getPrices() {
+    const { startDate, endDate } = this.state;
+    console.log(startDate);
     coinDesk.getPriceHistory(startDate, endDate, results => {
       const prices = results.data.bpi;
       this.setState({ prices });
@@ -23,6 +39,11 @@ export default class App extends React.Component {
 
   render() {
     const { prices } = this.state;
-    return <PriceChart prices={prices} />;
+    return (
+      <div>
+        {/* <DateSelection handleUpdate={this.updateDates} /> */}
+        <PriceChart prices={prices} />
+      </div>
+    );
   }
 }
