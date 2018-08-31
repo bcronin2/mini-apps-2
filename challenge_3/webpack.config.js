a85ac8a19fa83c76a9e4607e2bea211dd4261e33
Ben Cronin <benc@Bens-MacBook-Pro.local>
2018-08-31 14:15:10 -0700

    CInstall dependencies and configure hello world

---
+++ challenge_3/webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+const ENTRY = path.resolve(__dirname, 'client/app.jsx');
+const OUTPUT = { path: path.resolve(__dirname, 'public'), filename: 'app.js' };
+const MODULE = {
+  rules: [
+    {
+      test: /\.jsx?$/,
+      use: {
+        loader: 'babel-loader',
+        options: { presets: ['@babel/react', '@babel/env'] }
+      }
+    },
+    {
+      test: /\.css$/,
+      use: [{ loader: 'style-loader' }, { loader: 'css-loader' }]
+    }
+  ]
+};
+
+module.exports = {
+  entry: ENTRY,
+  output: OUTPUT,
+  module: MODULE,
+  devtool: 'source-map',
+  watch: true
+};
