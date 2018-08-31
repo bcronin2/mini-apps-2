f16b540c0de99b7d0808fe94a2fbf571e2fe0cc7
Ben Cronin <benc@Bens-MacBook-Pro.local>
2018-08-31 14:47:15 -0700

    ChImprove messaging in app

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,4 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(<div>Hello, world!</div>, document.getElementById('container'));
+ReactDOM.render(
+  <div>HR99 is the best, and rebecca is *awesome*!!</div>,
+  document.getElementById('container')
+);
